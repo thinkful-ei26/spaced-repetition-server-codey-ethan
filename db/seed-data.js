d6c50517fdd6004a4a4083f2c2b2ac79ebad827c
Ethan Frigon <ethan.frigon@gmail.com>
2019-01-28 09:12:30 -0800

    mlab db connected, working on seeding users

---
+++ db/seed-data.js
@@ -0,0 +1,20 @@
+'use strict';
+
+const users = [
+  {
+    _id: '000000000000000000000001',
+    fullname: 'Ms Green',
+    username: 'msgreen',
+    // hash for "password"
+    password: '$2a$10$QJCIX42iD5QMxLRgHHBJre2rH6c6nI24UysmSYtkmeFv6X8uS1kgi'
+  },
+  {
+    _id: '000000000000000000000002',
+    fullname: 'Mr Yellow',
+    username: 'mryellow',
+    // hash for "password"
+    password: '$2a$10$QJCIX42iD5QMxLRgHHBJre2rH6c6nI24UysmSYtkmeFv6X8uS1kgi'
+  }
+];
+
+module.exports = { users };
---
+++ models/user.js
@@ -0,0 +1,42 @@
+'use strict';
+
+const mongoose = require('mongoose');
+
+const bcrypt = require('bcryptjs');
+
+const userSchema = new mongoose.Schema({
+  fullname: {
+    type: String,
+    required: true
+  },
+  username: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  progress: Number
+});
+
+userSchema.set('toJSON', {
+  virtuals: true,
+  transform: (doc, result) => {
+    delete result._id;
+    delete result.__v;
+    delete result.password;
+  }
+});
+
+userSchema.methods.validatePassword = function(incomingPassword) {
+  return bcrypt.compare(incomingPassword, this.password);
+};
+
+userSchema.statics.hashPassword = function (incomingPassword) {
+  const digest = bcrypt.hash(incomingPassword, 10);
+  return digest;
+};
+
+module.exports = mongoose.model('User', userSchema);
